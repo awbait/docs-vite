a672f5b95b9b5a6d14b6832d418d6716eb7c406d
awbait <awbait@users.noreply.github.com>
2022-10-08 23:41:08 +0000

    deploy: 9864cd4e843c6b1395e514cc3d0d9ae1a84670a1

---
+++ assets/software_databases_postgresql.md.37806692.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as a,o as n,d as l}from"./app.989d2221.js";const D=JSON.parse('{"title":"Postgresql","description":"","frontmatter":{},"headers":[],"relativePath":"software/databases/postgresql.md"}'),p={name:"software/databases/postgresql.md"},o=l("",15),e=[o];function c(C,r,t,A,y,i){return n(),a("div",null,e)}const E=s(p,[["render",c]]);export{D as __pageData,E as default};
