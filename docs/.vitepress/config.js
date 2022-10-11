0a743df57c7fc3546d34d05338a71e287152625e
Andrey Paliy <awbait.com@gmail.com>
2022-10-11 03:51:44 +0300

    v0.3.3 config edit

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   description: 'DevOps documentation',
 
   lastUpdated: true,
-  cleanUrls: 'without-subfolders',
+  // cleanUrls: 'without-subfolders',
 
   themeConfig: {
     nav: nav(),
@@ -27,14 +27,14 @@ export default defineConfig({
 function nav() {
   return [
     { text: 'DevOps', link: '/devops/info' },
-    {
-      text: 'Dropdown Menu',
-      items: [
-        { text: 'Item A', link: '/item-1' },
-        { text: 'Item B', link: '/item-2' },
-        { text: 'Item C', link: '/item-3' }
-      ]
-    }
+    // {
+    //   text: 'Dropdown Menu',
+    //   items: [
+    //     { text: 'Item A', link: '/item-1' },
+    //     { text: 'Item B', link: '/item-2' },
+    //     { text: 'Item C', link: '/item-3' }
+    //   ]
+    // }
   ]
 }
 
@@ -165,12 +165,12 @@ function sidebarMain() {
 function social() {
   return [
     { icon: 'github', link: 'https://github.com/awbait' },
-    { icon: 'twitter', link: '...' },
-    {
-      icon: {
-        svg: '<svg width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g><path fill="none" d="M0 0h24v24H0z"/><path d="M12 3a3 3 0 0 0-3 3v6a3 3 0 0 0 6 0V6a3 3 0 0 0-3-3zm0-2a5 5 0 0 1 5 5v6a5 5 0 0 1-10 0V6a5 5 0 0 1 5-5zM2.192 13.962l1.962-.393a8.003 8.003 0 0 0 15.692 0l1.962.393C20.896 18.545 16.85 22 12 22s-8.896-3.455-9.808-8.038z"/></g></svg>',
-      },
-      link: '...'
-    }
+    // { icon: 'twitter', link: '...' },
+    // {
+    //   icon: {
+    //     svg: '<svg width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g><path fill="none" d="M0 0h24v24H0z"/><path d="M12 3a3 3 0 0 0-3 3v6a3 3 0 0 0 6 0V6a3 3 0 0 0-3-3zm0-2a5 5 0 0 1 5 5v6a5 5 0 0 1-10 0V6a5 5 0 0 1 5-5zM2.192 13.962l1.962-.393a8.003 8.003 0 0 0 15.692 0l1.962.393C20.896 18.545 16.85 22 12 22s-8.896-3.455-9.808-8.038z"/></g></svg>',
+    //   },
+    //   link: '...'
+    // }
   ]
 }
